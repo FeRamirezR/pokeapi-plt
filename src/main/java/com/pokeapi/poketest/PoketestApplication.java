f59166a1e055600f75f9148264a0075be7a2b3e6
FeRamirezR <lframirezr4@gmail.com>
2025-04-10 18:35:53 -0500

    Proyecto LT C1

---
+++ src/main/java/com/pokeapi/poketest/PoketestApplication.java
@@ -0,0 +1,13 @@
+package com.pokeapi.poketest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoketestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PoketestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pokeapi/poketest/PoketestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pokeapi.poketest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoketestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
